1ce994760e44dc169f71c728443f2ef20620e8c3
Deborah Ong <deborah.oyt@gmail.com>
2021-12-31 19:28:43 -0800

    add pry file

---
+++ pry.rb
@@ -0,0 +1,12 @@
+require 'pry'
+
+favorite_things = ["Trapper Keeper", "Netscape Navigator", "Troll Doll"]
+
+binding.pry
+
+new_thing = "Banana Split"
+
+favorite_things << new_thing
+
+binding.pry
+#to exit type !!!
